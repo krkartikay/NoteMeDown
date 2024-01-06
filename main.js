d8713be596685f9f1e1f09f366cc2336e6c52097
Kumar Kartikay <krkartikay@gmail.com>
2024-01-06 18:29:21 +0530

    first iteration

---
+++ main.js
@@ -0,0 +1,30 @@
+function CreateNewNote() {
+    // get note entered and clear it
+    var cmd_bar = $("#cmd_bar")
+    var note = cmd_bar.val()
+    cmd_bar.val("")
+    // now we gotta show it
+    var notes = $("#notes")
+    var new_note = `<li>${note}</li>`
+    notes.append(new_note)
+}
+
+function Refocus() {
+    $("#cmd_bar").focus()
+}
+
+function onDocumentLoad() {
+    // set up evt listeners
+    $("#cmd_bar").keypress(onCmdBarKeyPress)
+    // focus cmd bar
+    Refocus()
+}
+
+function onCmdBarKeyPress(evt) {
+    if (evt.key == "Enter") {
+        CreateNewNote()
+    }
+    Refocus()
+}
+
+$(onDocumentLoad)
